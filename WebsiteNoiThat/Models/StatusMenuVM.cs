b33a2b286943663fb2274a483f62c0fa7921aa0b
thuong3011 <163385553+thuong3011@users.noreply.github.com>
2026-01-02 14:41:17 +0700

    Add files via upload

---
+++ WebsiteNoiThat/Models/StatusMenuVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteNoiThat.Models
+{
+	public class StatusMenuVM
+	{
+		public int StatusId { get; set; }
+		public string Name { get; set; }
+		public int Count { get; set; }
+	}
+
+}
