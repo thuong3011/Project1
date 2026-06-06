e82c056b7c51c846db259ba1dc5cbb91efa78b66
thuong3011 <163385553+thuong3011@users.noreply.github.com>
2026-06-06 10:27:59 +0700

    Add files via upload

---
+++ Models/EF/SocialAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Models.EF
+{
+	public class SocialAccount
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public int UserId { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string Provider { get; set; }
+
+		[Required]
+		[StringLength(255)]
+		public string ProviderUserId { get; set; }
+
+		[ForeignKey("UserId")]
+		public virtual User User { get; set; }
+	}
+}
